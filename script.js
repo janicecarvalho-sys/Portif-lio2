e35699eb4e7d615b6a7e622ea033adc680458970
janicecarvalho-sys <janice.carvalho@escola.pr.gov.br>
2025-11-17 19:28:29 -0300

    if

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ let texto = document.querySelector("#texto");
 texto.textContent = "Janice";
 
 let nome = prompt("Qual o seu nome?");
-texto.textContent = nome;
+if (texto.textContent == null){
+    texto.textContent = "Seja bem-vindo(a)!"
+}}
+else{
+    texto.textContent = nome;
+}
+
+
+
